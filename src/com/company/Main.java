d01140edb60507e29303e1d233dfd1423d93fe33
kazsojoe <zsolt.kadar.info@gmail.com>
2020-05-15 22:35:39 +0200

    Second commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("This id my first project on Github!");
-        System.out.println("This id my first project on Github!");
+        System.out.println("This is my first project on Github!");
+        System.out.println("This is my first project on Github!");
     }
 }
