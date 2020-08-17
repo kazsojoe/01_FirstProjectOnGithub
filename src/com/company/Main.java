3718172674449f0e831059ea3677688ff1143062
zsolt <zsolt.kadar.info@gmail.com>
2020-08-17 23:43:59 +0200

    Third commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("This is my first project on Github!");
         System.out.println("This is my first project on Github!");
+        System.out.println("This is my first project on Github!");
     }
 }
